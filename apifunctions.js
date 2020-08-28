8dd8eb0231f381fcc51f61f367b29b42c62805e5
ParvishGajjar <parvishgajjar@gmail.com>
2020-08-28 18:01:53 +0530

    Done with post api but can't make it work as not able to make connection as global variable

---
+++ apifunctions.js
@@ -0,0 +1,43 @@
+// var connection=window.connection
+import {connection} from './index.js'
+function getCity(req,res){
+    var sid=req.params.id;
+    connection.query(`select * from city where STATE_ID=${sid};`,(err,rows)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(rows);
+        res.end(JSON.stringify(rows));
+    })
+}
+
+function getState(req,res){
+    var cid=req.params.id;
+    connection.query(`select * from state where COUNTRY_ID=${cid};`,(err,rows)=>{
+        if(err){
+            console.log(err)
+        }
+        console.log(rows);
+        res.end(JSON.stringify(rows))
+    })
+}
+function getCountry(req,res){
+    connection.query(`select * from country;`,(err,rows)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(rows)
+        res.end(JSON.stringify(rows));
+    })
+}
+function insertLocation(req,res){
+    connection.query(`insert into emploc (EMP_ID,COUNTRY_ID,STATE_ID,CITY_ID) values (${req.body.empid},${req.body.countryid},${req.body.stateid},${req.body.cityid});`,(err,results)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(results)
+        res.json.send(results);
+    })
+}
+
+export { getCity, getState, getCountry, insertLocation }
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import * as api from './apifunctions.js'
 import mysql from 'mysql'
 var app = express()
 
@@ -8,7 +9,7 @@ var connection = mysql.createConnection({
     password: "",
     database: "workone",
   });
-  
+
 connection.connect(function (err) {
     if (err) {
       return console.error("error: " + err.message);
@@ -17,43 +18,16 @@ connection.connect(function (err) {
     console.log("Connected to the MySQL server.");
 });
   
-app.get('/api/getcountry',getCountry)
-app.get('/api/getstate/:id',getState)
-app.get('/api/getcity/:id', getCity)
+app.get('/api/getcountry',api.getCountry)
+app.get('/api/getstate/:id',api.getState)
+app.get('/api/getcity/:id', api.getCity)
+app.post('/api/insertlocation',api.insertLocation)
 
-function getCity(req,res){
-    var sid=req.params.id;
-    connection.query(`select * from city where STATE_ID=${sid};`,(err,rows)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log(rows);
-        res.end(JSON.stringify(rows));
-    })
-}
-
-function getState(req,res){
-    var cid=req.params.id;
-    connection.query(`select * from state where COUNTRY_ID=${cid};`,(err,rows)=>{
-        if(err){
-            console.log(err)
-        }
-        console.log(rows);
-        res.end(JSON.stringify(rows))
-    })
-}
-function getCountry(req,res){
-    connection.query(`select * from country;`,(err,rows)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log(rows)
-        res.end(JSON.stringify(rows));
-    })
-}
 app.listen(3400, (err)=>{
     if(err){
         console.log(err);
     }
     console.log(`Server is listening on port 3400`);
 })
+// export {}
+export { connection };
